ccd98e8c95032c3a5221fc85259640e9fca9a299
Prince sharma <connect.princesharma@gmail.com>
2025-05-20 15:09:31 +0530

    minor fixes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,9 +20,9 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center gap-4 w-full h-screen bg-gray-200'>
       {open ? <CreateBoard handleOpenClose={handleOpenClose} /> : null}
-      <div className='w-full flex items-center bg-amber-600/70 p-2 justify-between px-32'>
+      <div className='w-full flex items-center bg-amber-600/70  p-2 justify-center '>
         <h1 className='text-3xl font-bold p-2 text-center'>Kanban App</h1>
-        <img src="" alt="" className='bg-amber-100 h-full w-13 rounded-full cursor-pointer'/>
+        {/* <img src="" alt="" className='bg-amber-100 h-full w-13 rounded-full cursor-pointer'/> */}
       </div>
       <button onClick={() => handleOpenClose()} className='bg-blue-400 font-[500] lg:text-md p-1 px-2 lg:p-1.5 lg:px-3 xl:p-2 xl:px-4 cursor-pointer rounded-lg '>Add a Board</button>
       <span className='text-xl font-[700]'>Your Boards: </span>
